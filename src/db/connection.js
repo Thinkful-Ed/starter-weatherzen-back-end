6d2b4f834dccb61e4d93673ecfb93ec8d7fb85c6
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2020-12-10 07:16:50 -0600

    style: remove console.log

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,6 +2,4 @@ const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
-console.debug("connection.js", environment, config)
-
 module.exports = knex;
